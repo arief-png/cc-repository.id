db25d52d771965f7f0f3551987bcc58cf65e0d05
arief-png <ariefalsulthan321@gmail.com>
2025-01-02 12:22:10 +0400

    computer club

---
+++ ccweb/cc.js
@@ -0,0 +1,18 @@
+function toggleTheme(){
+    const body = document.body
+    body.classList.toggle('light-mode');
+}<!-- 
+<div class="bungkus3">
+    <section id="join-us">
+        <h2 class="join">Join Us</h2>
+        <p class="joinP">Want to be part of our team? Join us now!</p>
+    </section>
+</div>
+
+<div class="bungkus4">
+    <section id="contact">
+        <h2 class="contact">Contact</h2>
+        <p class="contactP">Feel free to reach out to us via our contact page.</p>
+    </section>
+</div>
+</div>  -->
